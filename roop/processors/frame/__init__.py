7676a9dc36631b2798aef473acbc185dfbcb54f4
bimajavasteel <106569865+bimajavasteel@users.noreply.github.com>
2025-11-19 08:00:33 +0700

    Update __init__.py

--- roop/processors/frame/__init__.py
+++ roop/processors/frame/__init__.py
@@ -1,7 +1,1 @@
-from .face_swapper import process_frames as process_face_swapper
-from .face_enhancer import process_frames as process_face_enhancer
 
-FRAME_PROCESSORS = {
-    "face_swapper": process_face_swapper,
-    "face_enhancer": process_face_enhancer,
-}
