5ec545ad8654d898e8788ffd48ae3c93143d3da0
bimajavasteel <106569865+bimajavasteel@users.noreply.github.com>
2025-11-22 18:55:34 +0700

    Update face_swapper.py

--- roop/processors/frame/face_swapper.py
+++ roop/processors/frame/face_swapper.py
@@ -6,7 +6,7 @@ import threading
 import roop.globals
 import roop.processors.frame.core
 from roop.core import update_status
-from roop.face_analyser import get_one_face, get_many_faces, find_similar_face
+from roop.face_analyser import get_one_face, get_many_faces
 from roop.face_reference import get_face_reference, set_face_reference, clear_face_reference
 from roop.typing import Face, Frame
 from roop.utilities import conditional_download, resolve_relative_path, is_image, is_video
