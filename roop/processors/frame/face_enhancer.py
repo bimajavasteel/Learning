761a72e4f93f80efb630556c7f26e72dfbe2c9ce
bimajavasteel <106569865+bimajavasteel@users.noreply.github.com>
2025-11-30 17:12:42 +0700

    Update face_enhancer.py

--- roop/processors/frame/face_enhancer.py
+++ roop/processors/frame/face_enhancer.py
@@ -10,6 +10,7 @@ from roop.core import update_status
 from roop.face_analyser import get_many_faces
 from roop.typing import Frame, Face
 from roop.utilities import conditional_download, resolve_relative_path, is_image, is_video
+from roop.processors.frame.perlin_skin_noise import add_subtle_skin_noise
 
 FACE_ENHANCER = None
 THREAD_SEMAPHORE = threading.Semaphore()
